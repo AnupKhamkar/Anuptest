f2a876648908f14de4e519ede42cd09bd1469912
AnupKhamkar <anup.khamkar@accenture.com>
2026-02-24 06:46:46 +0000

    Update Django settings and URLs for Codespace API endpoint support and security

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-q9!969tqmp&9gc-)3k$swm6vu3240gh3zp*iesdwoa8_-5ea$a
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
